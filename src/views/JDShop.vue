ae3f14828c0d9beee1d0e0877cb02dbe11ee646a
woq <33190773+woq@users.noreply.github.com>
2021-09-10 21:34:31 +0800

    add Some

---
+++ src/views/JDShop.vue
@@ -0,0 +1,26 @@
+<template>
+ <h1>jd shop</h1>
+  <input type="text" placeholder="搜索商品" name="shop" v-model="keyWord">
+  <button @click="openNewWindow(keyWord)">Some</button>
+</template>
+
+<script>
+export default {
+  name: "JDShop",
+  data(){
+    return{
+      keyWord: ""
+    }
+  },
+  methods:{
+    openNewWindow(searchText){
+      window.open('https://www.baidu.com/'+searchText)
+    }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
